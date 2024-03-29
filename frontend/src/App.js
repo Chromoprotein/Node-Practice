4f0e496691e1610d9e4591121d415ed90c841a64
Chromoprotein <bridges.of.sighs@gmail.com>
2024-03-29 17:15:28 +0200

    server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [food, setFood] = useState([]);
+
+  useEffect(() => {
+    axios.get('/item')
+    .then(res => {
+      console.log(res.data.data)
+      setFood(res.data.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Food</th>
+            <th>Opinion</th>
+          </tr>
+        </thead>
+        <tbody>
+          {food.map(item => {
+            return (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.food}</td>
+                <td>{item.opinion}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </React.Fragment>
   );
 }
 
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const Item = require('./Item');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+//Cors should prevent CORS errors. If they happen anyway, clearing the cache works
+app.use(cors());
+app.use(express.json());
+
+require('./db')
+
+app.get("/item", (req,res) => {
+    Item
+        .find()
+        .then((data) => {
+            return res.status(200).json({
+                data
+            })
+        })
+        .catch((err) => {
+            return res.status(400).json({
+                err
+            })
+        })
+
+})
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, 'frontend/build')));
+
+//This one should be the last
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'));
+});
+
+//process.env.PORT for deployment, 8000 for local development
+app.listen(process.env.PORT || 8000, () => {
+    console.log("server started on port 8000");
+});
